a9eedb96a0e6597a7e80de9c30b0a47ae11cc2e0
moorthybasava <moorthybasava@hotmail.com>
2020-06-07 15:21:28 +0530

    Added a dao folder to the quora-service module -created the UserDao class to process all the database requests from UserBusinessService -included functionality to check for username and emailaddress in the users table -included functionality to create a new uesr entity in the users table -with this the signUpUser endpoint is completely implemented

---
+++ quora-service/src/main/java/com/upgrad/quora/service/dao/UserDao.java
@@ -0,0 +1,52 @@
+package com.upgrad.quora.service.dao;
+
+import com.upgrad.quora.service.entity.UserAuthTokenEntity;
+import com.upgrad.quora.service.entity.UserEntity;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.persistence.PersistenceContext;
+
+@Repository
+public class UserDao {
+
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    public UserEntity createUser(UserEntity userEntity) {
+        entityManager.persist(userEntity);
+        return  userEntity;
+    }
+
+    public UserEntity getUserByEmail(final String email) {
+
+        try {
+            return entityManager.createNamedQuery("userByEmail", UserEntity.class).setParameter("email", email).getSingleResult();
+        } catch (NoResultException nre){
+            return null;
+        }
+    }
+
+    public UserEntity checkUsername(String userName) {
+
+        try{
+            return entityManager.createNamedQuery("checkUserName",UserEntity.class).setParameter("username",userName).getSingleResult();
+
+        }catch(NoResultException nre){
+            return null;
+        }
+    }
+
+    public UserEntity checkEmailaddress(String emailAddress) {
+        try{
+            return entityManager.createNamedQuery("userByEmail",UserEntity.class).setParameter("email",emailAddress).getSingleResult();
+
+        }catch(NoResultException nre){
+            return null;
+        }
+    }
+
+
+}
