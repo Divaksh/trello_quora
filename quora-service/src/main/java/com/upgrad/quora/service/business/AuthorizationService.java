49151ba5a590353401a70f9291dd18bcc74e2878
moorthybasava <moorthybasava@hotmail.com>
2020-06-07 16:25:59 +0530

    Added business logic to AuthorizationService for the signOut request -implemented the AuthorizationService.authenticateUserRequest method -also added the UserRequestDetails class which helps in better processing the user-request -implemented the getUserRequestDetails method which is used to modularize the authorizationservice

--- quora-service/src/main/java/com/upgrad/quora/service/business/AuthorizationService.java
+++ quora-service/src/main/java/com/upgrad/quora/service/business/AuthorizationService.java
@@ -27,18 +27,18 @@ public class AuthorizationService {
     public void validateUserSignUpRequest(final String searchByString, final String requestDetails) throws SignUpRestrictedException {
         new UserEntity();
         UserEntity validateUserSignUp;
-        switch(requestDetails) {
+        switch (requestDetails) {
 
             case "validate-username":
                 validateUserSignUp = userDao.checkUsername(searchByString);
-                if(validateUserSignUp != null){
-                    throw new SignUpRestrictedException("SGR-001","Try any other Username, this Username has already been taken");
+                if (validateUserSignUp != null) {
+                    throw new SignUpRestrictedException("SGR-001", "Try any other Username, this Username has already been taken");
                 }
                 break;
             case "validate-emailaddress":
                 validateUserSignUp = userDao.checkEmailaddress(searchByString);
-                if(validateUserSignUp != null){
-                    throw new SignUpRestrictedException("SGR-002","This user has already been registered, try with any other emailId");
+                if (validateUserSignUp != null) {
+                    throw new SignUpRestrictedException("SGR-002", "This user has already been registered, try with any other emailId");
                 }
                 break;
             default:
@@ -46,6 +46,34 @@ public class AuthorizationService {
         }
     }
 
+
+    public UserAuthTokenEntity authenticateUserRequest(final String accessToken, String requestDetailsString) throws SignUpRestrictedException {
+        UserRequestDetails userRequestDetails = getUserRequestDetails(accessToken, requestDetailsString);
+        UserAuthTokenEntity userSignedIn = userRequestDetails.getUserAuthTokenEntity();
+        String request = userRequestDetails.getRequest();
+        boolean notSignedIn = userRequestDetails.isUserNotSignedIn();
+        boolean signedOut = userRequestDetails.isUserSignedOut();
+        if (request.equalsIgnoreCase("sign-out-request")) {
+            if (notSignedIn || signedOut) {
+                throw new SignUpRestrictedException("SGR-001", "User is not signed in");
+            }
+        }
+        return userSignedIn;
+
+    }
+    public UserRequestDetails getUserRequestDetails(final String accessToken,final String requestDetailsString){
+        String [] requestDetails =requestDetailsString.split("@ID ",2);
+        String request=requestDetails[0];
+        String searchById=requestDetails[1];
+        UserAuthTokenEntity userSignedIn= userDao.checkUserStatus(accessToken);
+        ZonedDateTime timeNow = ZonedDateTime.now();
+        boolean userNotSignedIn,userSignedOut;
+        userNotSignedIn=userSignedOut=false;
+        if(userSignedIn==null) userNotSignedIn=true;
+        if(((!userNotSignedIn) && (userSignedIn.getLogoutAt() != null)) || (!userNotSignedIn) && (timeNow.isAfter(userSignedIn.getExpiresAt()))) userSignedOut=true;
+        return new UserRequestDetails(userSignedIn,request,searchById,userNotSignedIn,userSignedOut);
+
+    }
 }
 
 
