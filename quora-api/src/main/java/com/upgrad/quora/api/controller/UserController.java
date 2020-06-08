835a48f04c50de4bb980645eb70b3f501ff2c308
moorthybasava <moorthybasava@hotmail.com>
2020-06-08 13:43:07 +0530

    Revert "Merge branch 'master' into user-functionality"
    This reverts commit ed55216c824c2d58045944061c2b140a15ccef14, reversing
    changes made to 7f7e823e3e683e25738ef5822b7eecd22aa319a1.
    
    Reverting the Merge on user-functionality from master
    --by mistake or lack of experience-merged my master into  user-functionality branch
    --reverting the same

--- quora-api/src/main/java/com/upgrad/quora/api/controller/UserController.java
+++ quora-api/src/main/java/com/upgrad/quora/api/controller/UserController.java
@@ -30,14 +30,6 @@ public class UserController {
     @Autowired
     private UserBusinessService userBusinessService;
 
-
-    //controller method for handling the userSignUp endpoint/request
-    //receives the SignupUserRequest object and returns the SignupUserResponse as a Response Entity
-    //validates the username and email address entered by the user and creates a new UserEntity with info received if valid
-    //creates the new UserEntity by passing on the user info to the UserBusinessService business logic layer
-    //returns the appropriate message along with the id of the UserEntity created in the response entity,and also httpstatus
-
-
     @RequestMapping(method = RequestMethod.POST, path = "user/signup", consumes = MediaType.APPLICATION_JSON_UTF8_VALUE, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public ResponseEntity<SignupUserResponse> userSignup(final SignupUserRequest signupUserRequest) throws SignUpRestrictedException {
         userBusinessService.validateUserSignUpRequest(signupUserRequest.getUserName(),signupUserRequest.getEmailAddress());
@@ -58,15 +50,6 @@ public class UserController {
         SignupUserResponse userResponse = new SignupUserResponse().id(createdUserEntity.getUuid()).status("USER SUCCESSFULLY REGISTERED");
         return new ResponseEntity<>(userResponse, HttpStatus.CREATED);
     }
-
-
-    //controller method for the userSiginIn endpoint/request
-    //receives the authorization as basic authorization,throws AuthenticationFailedException if invalid credentials provided
-    //decodes the base64 encoded authorization string and validates the username and password provided by the user
-    //successfully validates and then creates accesstoken for the user by accessing the userBusinessservice layer
-    //returns the accesstoken and the appropriate message in the response entity
-
-
     @RequestMapping(method = RequestMethod.POST, path = "/user/signin", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public ResponseEntity<SigninResponse> userSignIn(@RequestHeader("authorization") final String authorization) throws AuthenticationFailedException {
         byte[] decode = Base64.getDecoder().decode(authorization.split("Basic ")[1]);
@@ -83,14 +66,6 @@ public class UserController {
         headers.add("access-token", userAuthToken.getAccessToken());
         return new ResponseEntity<>(signinResponse, headers, HttpStatus.OK);
     }
-
-
-    //controller method for the UserSignOut endpoint/request
-    //receives accesstoken as a bearer authorization and returns response entity
-    //authenticates user request-throws the signuprestrictedexception
-    //if user is logged in then logs him out and sends the signoutresponse with the appropriate message and http status
-
-
     @RequestMapping(method = RequestMethod.POST , path="/user/signout" ,produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public ResponseEntity<SignoutResponse> userSignOut(@RequestHeader("authorization")  final String authorization) throws SignUpRestrictedException{
         String accessToken=authorization.split("Bearer ")[1];
