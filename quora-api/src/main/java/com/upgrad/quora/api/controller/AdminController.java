848abc14e01ec277ef546b36505f79e156b2fdf4
moorthybasava <moorthybasava@hotmail.com>
2020-06-08 14:43:38 +0530

    Added comments to all the controller,service and dao classes -commented all the code

--- quora-api/src/main/java/com/upgrad/quora/api/controller/AdminController.java
+++ quora-api/src/main/java/com/upgrad/quora/api/controller/AdminController.java
@@ -19,6 +19,17 @@ public class AdminController {
     @Autowired
     private AdminBusinessService adminBusinessService;
 
+    //controller method for the deleteUser endpoint /request
+    //receives the userId of the user whose details are to be deleted along with the accesstoken of the user trying to delete
+    //validates the signin status of the user making the request and also the role of the user trying to delete the user record
+    //throws the authenticationfailedexception if userr is not signed in or if the user is not admin
+    //throws the usernotfoundexception if the user deatails with the given usrrId is not found in the db
+    //deletes the user record from the users table if no exceptions are thrown
+    //returns the id of the user deleted and also appropriate message along with the http status
+    //returns the response entity obj after creating the userdeleteresponse with the above info
+
+
+
     @RequestMapping(method = RequestMethod.DELETE, path="admin/user/{userId}",produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public ResponseEntity<UserDeleteResponse> deleteUser(@RequestHeader("authorization")  final String authorization, @PathVariable("userId") final String userId) throws AuthenticationFailedException, UserNotFoundException {
         String accessToken=authorization.split("Bearer ")[1];
--- quora-api/src/main/java/com/upgrad/quora/api/controller/CommonController.java
+++ quora-api/src/main/java/com/upgrad/quora/api/controller/CommonController.java
@@ -19,6 +19,16 @@ public class CommonController {
     @Autowired
     private CommonBusinessService commonBusinessService;
 
+    //controller method for the getUserDetails endpoint
+   //receives the userId of the user whose details are to be fetched along with accesstoken of the user trying to fetch them
+   //this method is available to all the users of the application
+   //validates the user logged in by authenticating the access token passed in the authorization
+   //throws the authenticationFailedException if not user is not signed in
+   //throws the usernotfoundException if the user with the given userId is not found in the db
+   //returns the user details if no exception thrown in the response entity after creating a userdetailsresponse object
+   //along with the http status
+
+
     @RequestMapping(method = RequestMethod.GET, path="/userprofile/{userId}",produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public ResponseEntity<UserDetailsResponse> getUserDetails(@RequestHeader("authorization")  final String authorization, @PathVariable("userId") final String userId) throws AuthenticationFailedException, UserNotFoundException{
         String accessToken=authorization.split("Bearer ")[1];
